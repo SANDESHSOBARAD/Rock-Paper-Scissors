4542c9ff031c92cdb82d34ebe8de2f1b9ff4ba76
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2024-09-17 12:19:19 -0700

    button functionalities working

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
-let rock = document.querySelector('.rock')
-let paper = document.querySelector('.paper')
-let scissors = document.querySelector('.scissors')
+
 let restart = document.querySelector('.restart')
 let playerScore = document.querySelector('.player-score')
 let computerScore = document.querySelector('computer-score')
+let choices = document.querySelectorAll('.choice')
+let result = document.querySelector('.result')
 
-let count = 0
-rock.addEventListener('click', () =>{
-    console.log("you clicked on rock")
-    playerScore.innerHTML = ++count
-    console.log(count)
+let pScore = 0
+let cScore = 0
 
-})
-
-paper.addEventListener('click', () =>{
-    console.log("you clicked on paper")
-})
-
-scissors.addEventListener('click', () =>{
-    console.log("you clicked on scissors")
+const playGame = (playerChoice) => {
+    console.log(`The player chose ${playerChoice}`)
+}
+choices.forEach((choice) => {
+    choice.addEventListener('click', () => {
+        const playerChoice = choice.getAttribute("id");
+        playGame(playerChoice)
+    })
 })
 
 restart.addEventListener('click', () =>{
-    console.log("you clicked on restart game")
+    console.log('restarting the game')
 })
-
